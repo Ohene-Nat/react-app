05311e5bcc9625357f0888dd6613038aa6d9b8d9
Nathaniel <186203419+Ohene-Nat@users.noreply.github.com>
2026-04-30 20:05:26 +0000

    Render Food and multiple Card components in App
    Import Food and Card in App.jsx and render a <Food /> plus three <Card /> instances in the app output. Also fix the Header import semicolon. Integrates the new components into the main layout.

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,15 @@
 import Footer from "./footer";
-import Header from "./header"
+import Header from "./header";
+import Food from "./food";
+import Card from "./card";
 function App() {
   return (
     <div>
       <Header />
+      <Food />
+      <Card />
+      <Card />
+      <Card />
       <Footer />
     </div>
   );
